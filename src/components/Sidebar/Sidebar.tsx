d41851a254b106d94428fbe10b2abd42b0367fce
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2024-01-23 16:12:03 -0300

    chore: set expanded false on onMouseLeave

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -33,7 +33,7 @@ export const Sidebar = () => {
             setExpanded(true)
          }}
          onMouseLeave={() => {
-            setExpanded(true)
+            setExpanded(false)
          }}
       >
          <MenuCollapseBtn {...SidebarConfig.MenuCollapseBtnConfig} />
